a24d2fa39917bac4bf8bf9e3408cb85ca6eed9b3
Eder Nina <enina@barrick.com>
2020-09-04 20:03:38 -0400

    build update

---
+++ server.compiled.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var express = require('express');
+
+var favicon = require('express-favicon');
+
+var path = require('path');
+
+var port = process.env.PORT || 2020;
+var app = express();
+app.use(favicon(__dirname + '/build/favicon.ico')); // the __dirname is the current directory from where the script is running
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
